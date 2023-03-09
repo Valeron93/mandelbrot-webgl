9c54d8a6be08bf8b87cb33652cc46816c156172b
Valeron93 <valera1800@gmail.com>
2023-03-09 23:08:09 +0100

    fix: resizing did not redraw the picture

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,44 @@ const canvas = document.querySelector<HTMLCanvasElement>("#canvas") as HTMLCanva
 const gl = canvas.getContext("webgl") as WebGLRenderingContext;
 const renderer = Renderer.create(gl) as Renderer;
 
-const onResize = () => {
+const state = {
+    x: 0,
+    y: 0,
+    zoom: 1,
+};
+
+
+function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     renderer.setViewportSize(canvas.width, canvas.height);
+    draw();
 }
 
-window.addEventListener("resize", onResize);
-onResize();
-
-const translation = {
-    x: 0,
-    y: 0,
-};
-let zoom = 1;
-
 renderer.setZoom(1);
+
 function zoomBy(delta: number) {
     const sens = 0.0005;
-    zoom += delta * sens * zoom;
     
-    if (zoom < 0.000022) {
-        zoom = 0.000022;
+    state.zoom += delta * sens * state.zoom;
+    
+    if (state.zoom < 0.000022) {
+        state.zoom = 0.000022;
     }
 
-    if (zoom > 1) {
-        zoom = 1;
+    if (state.zoom > 1) {
+        state.zoom = 1;
     }
 
-    renderer.setZoom(zoom);
+    renderer.setZoom(state.zoom);
     draw();
+}
+
+
+
+function draw() {
+    renderer.clear();
+    renderer.drawRect();
 }
 
 window.addEventListener("wheel", (event) => {
@@ -47,18 +55,15 @@ window.addEventListener("mousemove", (event) => {
     }
 
     const { movementX, movementY } = event;
-
-    translation.x += zoom * movementX * 0.005;
-    translation.y -= zoom * movementY * 0.005;
-    renderer.setTranslation(translation.x, translation.y);
+    const { zoom } = state;
+    state.x += zoom * movementX * 0.005;
+    state.y -= zoom * movementY * 0.005;
+    renderer.setTranslation(state.x, state.y);
     draw();
 });
 
-function draw() {
-    renderer.clear();
-    renderer.drawRect();
-}
+window.addEventListener("resize", resize);
 
+resize();
 draw();
-
 
